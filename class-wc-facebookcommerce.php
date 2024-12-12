d98a99846a122d2cfc2595102a53c804cc13d324
Atul Dusane <atuldusane@fb.com>
2024-12-12 22:14:53 +0000

    created new fb checkout endpoint and included code for supporting multiple product ids and coupon via checkout

--- class-wc-facebookcommerce.php
+++ class-wc-facebookcommerce.php
@@ -90,6 +90,9 @@ class WC_Facebookcommerce extends WooCommerce\Facebook\Framework\Plugin {
 	/** @var WooCommerce\Facebook\Handlers\WebHook webhook handler */
 	private $webhook_handler;
 
+	/** @var WooCommerce\Facebook\Handlers\WebHook webhook handler */
+	private $checkout_handler;
+
 	/** @var WooCommerce\Facebook\Commerce commerce handler */
 	private $commerce_handler;
 
@@ -213,6 +216,7 @@ class WC_Facebookcommerce extends WooCommerce\Facebook\Framework\Plugin {
 
 			$this->connection_handler = new WooCommerce\Facebook\Handlers\Connection( $this );
 			$this->webhook_handler    = new WooCommerce\Facebook\Handlers\WebHook( $this );
+			$this->checkout_handler   = new WooCommerce\Facebook\Handlers\Checkout( $this );
 			$this->tracker            = new WooCommerce\Facebook\Utilities\Tracker();
 
 			// Init jobs
