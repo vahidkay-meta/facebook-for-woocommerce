a198d73952010e2ee26354537439ac2673ba091b
Carter Buce <carterbuce@gmail.com>
2025-01-29 17:38:56 -0800

    add delete request test

---
+++ tests/Unit/API/ProductCatalog/Products/Delete/RequestTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare( strict_types=1 );
+
+namespace API\ProductCatalog\Products\Delete;
+
+use WooCommerce;
+use WP_UnitTestCase;
+
+/**
+ * Test cases for product delete API request
+ */
+class RequestTest extends WP_UnitTestCase {
+	/**
+	 * Tests request endpoint config
+	 *
+	 * @return void
+	 */
+	public function test_request() {
+		$product_group_id = 'facebook-product-group-id';
+		$request          = new WooCommerce\Facebook\API\ProductCatalog\Products\Delete\Request( $product_group_id );
+
+		$this->assertEquals( 'DELETE', $request->get_method() );
+		$this->assertEquals( '/facebook-product-group-id', $request->get_path() );
+	}
+}
