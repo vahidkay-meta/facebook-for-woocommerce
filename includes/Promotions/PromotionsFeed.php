50afb5a2678c975298fc65c4db52bc10468b62a9
Julio Mendez Cabrera <jmencab@meta.com>
2025-02-06 11:35:30 -0800

    use new feed gen constructor, feed type const in feed constructor

--- includes/Promotions/PromotionsFeed.php
+++ includes/Promotions/PromotionsFeed.php
@@ -31,7 +31,7 @@ class PromotionsFeed extends AbstractFeed {
 	public function __construct() {
 		$data_stream_name = FeedType::PROMOTIONS;
 		$generator_factory    = facebook_for_woocommerce()->job_manager->generator_factory;
-		$this->feed_generator = $generator_factory->get_feed_generator( 'PromotionsFeedGenerator' );
+		$this->feed_generator = $generator_factory->get_feed_generator( $data_stream_name  );
 		$this->feed_handler   = new PromotionsFeedHandler( new CsvFeedFileWriter( $data_stream_name ) );
 		parent::__construct( $data_stream_name );
 	}
