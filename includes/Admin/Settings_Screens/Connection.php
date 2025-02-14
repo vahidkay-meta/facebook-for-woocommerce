08523588bb11a8b712c416e7cb357ccd7db589b2
Paul Kang <sollipse@meta.com>
2025-02-13 18:02:13 -0800

    Adding new rest endpoints to capture locally stored token and config data

--- includes/Admin/Settings_Screens/Connection.php
+++ includes/Admin/Settings_Screens/Connection.php
@@ -11,7 +11,7 @@
 
 namespace WooCommerce\Facebook\Admin\Settings_Screens;
 
-defined( 'ABSPATH' ) or exit;
+defined('ABSPATH') or exit;
 
 use WooCommerce\Facebook\Admin\Abstract_Settings_Screen;
 use WooCommerce\Facebook\Framework\Api\Exception as ApiException;
@@ -19,7 +19,8 @@ use WooCommerce\Facebook\Framework\Api\Exception as ApiException;
 /**
  * The Connection settings screen object.
  */
-class Connection extends Abstract_Settings_Screen {
+class Connection extends Abstract_Settings_Screen
+{
 
 	/** @var string screen ID */
 	const ID = 'connection';
@@ -31,22 +32,24 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @return bool
 	 */
-	protected function use_iframe_connection() {
+	protected function use_iframe_connection()
+	{
 		return true;
 	}
 
 	/**
 	 * Connection constructor.
 	 */
-	public function __construct() {
+	public function __construct()
+	{
 
 		$this->id    = self::ID;
-		$this->label = __( 'Connection', 'facebook-for-woocommerce' );
-		$this->title = __( 'Connection', 'facebook-for-woocommerce' );
+		$this->label = __('Connection', 'facebook-for-woocommerce');
+		$this->title = __('Connection', 'facebook-for-woocommerce');
 
-		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_assets' ) );
+		add_action('admin_enqueue_scripts', array($this, 'enqueue_assets'));
 
-		add_action( 'admin_notices', array( $this, 'add_notices' ) );
+		add_action('admin_notices', array($this, 'add_notices'));
 
 		// Add action to enqueue the message handler script
 		add_action('admin_footer', array($this, 'render_message_handler'));
@@ -60,19 +63,20 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function add_notices() {
+	public function add_notices()
+	{
 
 		// display a notice if the connection has previously failed
-		if ( get_transient( 'wc_facebook_connection_failed' ) ) {
+		if (get_transient('wc_facebook_connection_failed')) {
 
 			$message = sprintf(
 				/* translators: Placeholders: %1$s - <strong> tag, %2$s - </strong> tag, %3$s - <a> tag, %4$s - </a> tag, %5$s - <a> tag, %6$s - </a> tag */
-				__( '%1$sHeads up!%2$s It looks like there was a problem with reconnecting your site to Facebook. Please %3$sclick here%4$s to try again, or %5$sget in touch with our support team%6$s for assistance.', 'facebook-for-woocommerce' ),
+				__('%1$sHeads up!%2$s It looks like there was a problem with reconnecting your site to Facebook. Please %3$sclick here%4$s to try again, or %5$sget in touch with our support team%6$s for assistance.', 'facebook-for-woocommerce'),
 				'<strong>',
 				'</strong>',
-				'<a href="' . esc_url( facebook_for_woocommerce()->get_connection_handler()->get_connect_url() ) . '">',
+				'<a href="' . esc_url(facebook_for_woocommerce()->get_connection_handler()->get_connect_url()) . '">',
 				'</a>',
-				'<a href="' . esc_url( facebook_for_woocommerce()->get_support_url() ) . '" target="_blank">',
+				'<a href="' . esc_url(facebook_for_woocommerce()->get_support_url()) . '" target="_blank">',
 				'</a>'
 			);
 
@@ -84,7 +88,7 @@ class Connection extends Abstract_Settings_Screen {
 				)
 			);
 
-			delete_transient( 'wc_facebook_connection_failed' );
+			delete_transient('wc_facebook_connection_failed');
 		}
 	}
 
@@ -96,13 +100,14 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function enqueue_assets() {
+	public function enqueue_assets()
+	{
 
-		if ( ! $this->is_current_screen_page() ) {
+		if (! $this->is_current_screen_page()) {
 			return;
 		}
 
-		wp_enqueue_style( 'wc-facebook-admin-connection-settings', facebook_for_woocommerce()->get_plugin_url() . '/assets/css/admin/facebook-for-woocommerce-connection.css', array(), \WC_Facebookcommerce::VERSION );
+		wp_enqueue_style('wc-facebook-admin-connection-settings', facebook_for_woocommerce()->get_plugin_url() . '/assets/css/admin/facebook-for-woocommerce-connection.css', array(), \WC_Facebookcommerce::VERSION);
 	}
 
 
@@ -111,15 +116,16 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function render() {
+	public function render()
+	{
 
 		$is_connected = facebook_for_woocommerce()->get_connection_handler()->is_connected();
 
 		// always render the CTA box
-		$this->render_facebook_box( $is_connected );
+		$this->render_facebook_box($is_connected);
 
 		// don't proceed further if not connected
-		if ( ! $is_connected ) {
+		if (! $is_connected) {
 			return;
 		}
 
@@ -140,46 +146,46 @@ class Connection extends Abstract_Settings_Screen {
 		 */
 		$static_items = $this->use_iframe_connection() ? array() : array(
 			'page'                          => array(
-				'label' => __( 'Page', 'facebook-for-woocommerce' ),
+				'label' => __('Page', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_integration()->get_facebook_page_id(),
 			),
 			'pixel'                         => array(
-				'label' => __( 'Pixel', 'facebook-for-woocommerce' ),
+				'label' => __('Pixel', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_integration()->get_facebook_pixel_id(),
 			),
 			'catalog'                       => array(
-				'label' => __( 'Catalog', 'facebook-for-woocommerce' ),
+				'label' => __('Catalog', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_integration()->get_product_catalog_id(),
 				'url'   => 'https://facebook.com/products',
 			),
 			'business-manager'              => array(
-				'label' => __( 'Business Manager account', 'facebook-for-woocommerce' ),
+				'label' => __('Business Manager account', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_connection_handler()->get_business_manager_id(),
 			),
 			'ad-account'                    => array(
-				'label' => __( 'Ad Manager account', 'facebook-for-woocommerce' ),
+				'label' => __('Ad Manager account', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_connection_handler()->get_ad_account_id(),
 			),
 			'instagram-business-id'         => array(
-				'label' => __( 'Instagram Business ID', 'facebook-for-woocommerce' ),
+				'label' => __('Instagram Business ID', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_connection_handler()->get_instagram_business_id(),
 			),
 			'commerce-merchant-settings-id' => array(
-				'label' => __( 'Commerce Merchant Settings ID', 'facebook-for-woocommerce' ),
+				'label' => __('Commerce Merchant Settings ID', 'facebook-for-woocommerce'),
 				'value' => facebook_for_woocommerce()->get_connection_handler()->get_commerce_merchant_settings_id(),
 			),
 		);
 
 		// if the catalog ID is set, update the URL and try to get its name for display
 		$catalog_id = $static_items['catalog']['value'];
-		if ( !empty( $catalog_id ) ) {
+		if (!empty($catalog_id)) {
 			$static_items['catalog']['url'] = "https://www.facebook.com/commerce/catalogs/{$catalog_id}/products/";
 			try {
-				$response = facebook_for_woocommerce()->get_api()->get_catalog( $catalog_id );
-				if ( $name = $response->name ) {
+				$response = facebook_for_woocommerce()->get_api()->get_catalog($catalog_id);
+				if ($name = $response->name) {
 					$static_items['catalog']['value'] = $name;
 				}
-			} catch ( ApiException $exception ) {
+			} catch (ApiException $exception) {
 				// Log the exception with additional information
 				facebook_for_woocommerce()->log(
 					sprintf(
@@ -191,13 +197,13 @@ class Connection extends Abstract_Settings_Screen {
 			}
 		}
 
-		?>
+?>
 
 		<table class="form-table">
 			<tbody>
 
 				<?php
-				foreach ( $static_items as $id => $item ) :
+				foreach ($static_items as $id => $item) :
 
 					$item = wp_parse_args(
 						$item,
@@ -208,36 +214,35 @@ class Connection extends Abstract_Settings_Screen {
 						)
 					);
 
-					?>
+				?>
 
-					<tr valign="top" class="wc-facebook-connected-<?php echo esc_attr( $id ); ?>">
+					<tr valign="top" class="wc-facebook-connected-<?php echo esc_attr($id); ?>">
 
 						<th scope="row" class="titledesc">
-							<?php echo esc_html( $item['label'] ); ?>
+							<?php echo esc_html($item['label']); ?>
 						</th>
 
 						<td class="forminp">
 
-							<?php if ( $item['url'] ) : ?>
+							<?php if ($item['url']) : ?>
 
-								<a href="<?php echo esc_url( $item['url'] ); ?>" target="_blank">
+								<a href="<?php echo esc_url($item['url']); ?>" target="_blank">
 
-									<?php echo esc_html( $item['value'] ); ?>
+									<?php echo esc_html($item['value']); ?>
 
 									<span
 										class="dashicons dashicons-external"
-										style="margin-right: 8px; vertical-align: bottom; text-decoration: none;"
-									></span>
+										style="margin-right: 8px; vertical-align: bottom; text-decoration: none;"></span>
 
 								</a>
 
-							<?php elseif ( is_numeric( $item['value'] ) ) : ?>
+							<?php elseif (is_numeric($item['value'])) : ?>
 
-								<code><?php echo esc_html( $item['value'] ); ?></code>
+								<code><?php echo esc_html($item['value']); ?></code>
 
-							<?php elseif ( ! empty( $item['value'] ) ) : ?>
+							<?php elseif (! empty($item['value'])) : ?>
 
-								<?php echo esc_html( $item['value'] ); ?>
+								<?php echo esc_html($item['value']); ?>
 
 							<?php else : ?>
 
@@ -253,7 +258,7 @@ class Connection extends Abstract_Settings_Screen {
 			</tbody>
 		</table>
 
-		<?php
+	<?php
 
 		parent::render();
 	}
@@ -266,7 +271,8 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @param bool $is_connected whether the plugin is connected
 	 */
-	private function render_facebook_box( $is_connected ) {
+	private function render_facebook_box($is_connected)
+	{
 		if ($this->use_iframe_connection()) {
 			$this->render_facebook_box_iframe($is_connected);
 		} else {
@@ -281,23 +287,23 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @param bool $is_connected whether the plugin is connected
 	 */
-	private function render_facebook_box_iframe( $is_connected ) {
+	private function render_facebook_box_iframe($is_connected)
+	{
 		$connection = facebook_for_woocommerce()->get_connection_handler();
 		$iframe_url = \WooCommerce\Facebook\Handlers\MetaExtension::generateIframeSplashUrl(
 			$is_connected,
 			$connection->get_plugin(),
 			$connection->get_external_business_id()
 		);
-		?>
-		<iframe 
-			src="<?php echo esc_url( $iframe_url ); ?>" 
-			width="100%" 
-			height="600" 
-			frameborder="0" 
+	?>
+		<iframe
+			src="<?php echo esc_url($iframe_url); ?>"
+			width="100%"
+			height="600"
+			frameborder="0"
 			style="background: transparent;"
-			id="facebook-commerce-iframe"
-		></iframe>
-		<?php
+			id="facebook-commerce-iframe"></iframe>
+	<?php
 	}
 
 	/**
@@ -307,48 +313,49 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @param bool $is_connected whether the plugin is connected
 	 */
-	private function render_facebook_box_legacy( $is_connected ) {
-		if ( $is_connected ) {
-			$title = __( 'Reach the Right People and Sell More Online', 'facebook-for-woocommerce' );
+	private function render_facebook_box_legacy($is_connected)
+	{
+		if ($is_connected) {
+			$title = __('Reach the Right People and Sell More Online', 'facebook-for-woocommerce');
 		} else {
-			$title = __( 'Grow your business on Facebook', 'facebook-for-woocommerce' );
+			$title = __('Grow your business on Facebook', 'facebook-for-woocommerce');
 		}
 
-		$subtitle = __( 'Use this WooCommerce and Facebook integration to:', 'facebook-for-woocommerce' );
+		$subtitle = __('Use this WooCommerce and Facebook integration to:', 'facebook-for-woocommerce');
 		$benefits = array(
-			__( 'Create an ad in a few steps', 'facebook-for-woocommerce' ),
-			__( 'Use built-in best practices for online sales', 'facebook-for-woocommerce' ),
-			__( 'Get reporting on sales and revenue', 'facebook-for-woocommerce' ),
+			__('Create an ad in a few steps', 'facebook-for-woocommerce'),
+			__('Use built-in best practices for online sales', 'facebook-for-woocommerce'),
+			__('Get reporting on sales and revenue', 'facebook-for-woocommerce'),
 		);
 
-		?>
+	?>
 		<div id="wc-facebook-connection-box">
 			<div class="logo"></div>
-			<h1><?php echo esc_html( $title ); ?></h1>
-			<h2><?php echo esc_html( $subtitle ); ?></h2>
+			<h1><?php echo esc_html($title); ?></h1>
+			<h2><?php echo esc_html($subtitle); ?></h2>
 			<ul class="benefits">
-				<?php foreach ( $benefits as $key => $benefit ) : ?>
-					<li class="benefit benefit-<?php echo esc_attr( $key ); ?>"><?php echo esc_html( $benefit ); ?></li>
+				<?php foreach ($benefits as $key => $benefit) : ?>
+					<li class="benefit benefit-<?php echo esc_attr($key); ?>"><?php echo esc_html($benefit); ?></li>
 				<?php endforeach; ?>
 			</ul>
 			<div class="actions">
-				<?php if ( $is_connected ) : ?>
-					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_disconnect_url() ); ?>" class="button button-primary uninstall" onclick="return confirmDialog();">
-						<?php esc_html_e( 'Disconnect', 'facebook-for-woocommerce' ); ?>
+				<?php if ($is_connected) : ?>
+					<a href="<?php echo esc_url(facebook_for_woocommerce()->get_connection_handler()->get_disconnect_url()); ?>" class="button button-primary uninstall" onclick="return confirmDialog();">
+						<?php esc_html_e('Disconnect', 'facebook-for-woocommerce'); ?>
 					</a>
 					<script>
 						function confirmDialog() {
-							return confirm( 'Are you sure you want to disconnect from Facebook?' );
+							return confirm('Are you sure you want to disconnect from Facebook?');
 						}
 					</script>
 				<?php else : ?>
-					<a href="<?php echo esc_url( facebook_for_woocommerce()->get_connection_handler()->get_connect_url() ); ?>" class="button button-primary">
-						<?php esc_html_e( 'Get Started', 'facebook-for-woocommerce' ); ?>
+					<a href="<?php echo esc_url(facebook_for_woocommerce()->get_connection_handler()->get_connect_url()); ?>" class="button button-primary">
+						<?php esc_html_e('Get Started', 'facebook-for-woocommerce'); ?>
 					</a>
 				<?php endif; ?>
 			</div>
 		</div>
-		<?php
+	<?php
 	}
 
 	/**
@@ -356,33 +363,71 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @since 2.0.0
 	 */
-	public function render_message_handler() {
+	public function render_message_handler()
+	{
 		if (!$this->is_current_screen_page()) {
 			return;
 		}
-
-		?>
+	?>
 		<script type="text/javascript">
-			(function($) {
-				window.addEventListener('message', function(event) {
-					// Verify the message origin
-					console.log(event);
-					if (event.origin !== 'https://www.commercepartnerhub.com') {
-						return;
-					}
+			window.addEventListener('message', function(event) {
+				console.log('message', event);
+				if (event.origin !== 'https://www.commercepartnerhub.com') {
+					return;
+				}
 
-					const data = event.data;
-					
-					// Handle the access token if present
-					if (data && data.accessToken) {
-						// Send the token to our REST API endpoint
-						console.log(data)
-						alert('Access token received');
+				const message = event.data;
+				const messageEvent = message.event;
+
+				if (messageEvent === 'CommerceExtension::INSTALL' && message.success) {
+					fetch('/wp-json/wc-facebook/v1/update_tokens', {
+							method: 'POST',
+							credentials: 'same-origin',
+							headers: {
+								'Content-Type': 'application/json',
+								'X-WP-Nonce': '<?php echo wp_create_nonce("wp_rest"); ?>'
+							},
+							body: JSON.stringify({
+								access_token: message.access_token,
+								merchant_access_token: message.merchant_access_token,
+								page_access_token: message.page_access_token,
+								product_catalog_id: message.catalog_id,
+								pixel_id: message.pixel_id
+							})
+						})
+						.then(response => {
+							if (!response.ok) {
+								throw new Error('Network response was not ok');
+							}
+							return response.json();
+						})
+						.then(data => {
+							if (data.success) {
+								window.location.reload();
+							} else {
+								console.error('Failed to update Facebook settings:', data.message);
+							}
+						})
+						.catch(error => {
+							console.error('Error updating Facebook settings:', error);
+						});
+				}
+
+				// Handle iframe resize events
+				if (messageEvent === 'CommerceExtension::RESIZE') {
+					const iframe = document.getElementById('facebook-commerce-iframe');
+					if (iframe && message.height) {
+						iframe.height = message.height;
 					}
-				}, false);
-			})(jQuery);
+				}
+
+				// Handle uninstall/disconnect event
+				if (messageEvent === 'CommerceExtension::UNINSTALL') {
+					window.location.reload();
+				}
+			}, false);
 		</script>
-		<?php
+<?php
 	}
 
 
@@ -393,43 +438,42 @@ class Connection extends Abstract_Settings_Screen {
 	 *
 	 * @return array
 	 */
-	public function get_settings() {
+	public function get_settings()
+	{
 
 		return array(
 
 			array(
-				'title' => __( 'Debug', 'facebook-for-woocommerce' ),
+				'title' => __('Debug', 'facebook-for-woocommerce'),
 				'type'  => 'title',
 			),
 
 			array(
 				'id'       => \WC_Facebookcommerce_Integration::SETTING_ENABLE_DEBUG_MODE,
-				'title'    => __( 'Enable debug mode', 'facebook-for-woocommerce' ),
+				'title'    => __('Enable debug mode', 'facebook-for-woocommerce'),
 				'type'     => 'checkbox',
-				'desc'     => __( 'Log plugin events for debugging.', 'facebook-for-woocommerce' ),
+				'desc'     => __('Log plugin events for debugging.', 'facebook-for-woocommerce'),
 				/**
 				 * Translators: %s URL to the documentation page.
 				 */
-				'desc_tip' => sprintf( __( 'Only enable this if you are experiencing problems with the plugin. <a href="%s" target="_blank">Learn more</a>.', 'facebook-for-woocommerce' ), 'https://woocommerce.com/document/facebook-for-woocommerce/#debug-tools' ),
+				'desc_tip' => sprintf(__('Only enable this if you are experiencing problems with the plugin. <a href="%s" target="_blank">Learn more</a>.', 'facebook-for-woocommerce'), 'https://woocommerce.com/document/facebook-for-woocommerce/#debug-tools'),
 				'default'  => 'no',
 			),
 
 			array(
 				'id'       => \WC_Facebookcommerce_Integration::SETTING_ENABLE_NEW_STYLE_FEED_GENERATOR,
-				'title'    => __( 'Experimental! Enable new style feed generation', 'facebook-for-woocommerce' ),
+				'title'    => __('Experimental! Enable new style feed generation', 'facebook-for-woocommerce'),
 				'type'     => 'checkbox',
-				'desc'     => __( 'Use new, memory improved, feed generation process.', 'facebook-for-woocommerce' ),
+				'desc'     => __('Use new, memory improved, feed generation process.', 'facebook-for-woocommerce'),
 				/**
 				 * Translators: %s URL to the documentation page.
 				 */
-				'desc_tip' => sprintf( __( 'This is an experimental feature in testing phase. Only enable this if you are experiencing problems with feed generation. <a href="%s" target="_blank">Learn more</a>.', 'facebook-for-woocommerce' ), 'https://woocommerce.com/document/facebook-for-woocommerce/#feed-generation' ),
+				'desc_tip' => sprintf(__('This is an experimental feature in testing phase. Only enable this if you are experiencing problems with feed generation. <a href="%s" target="_blank">Learn more</a>.', 'facebook-for-woocommerce'), 'https://woocommerce.com/document/facebook-for-woocommerce/#feed-generation'),
 				'default'  => 'no',
 			),
 
-			array( 'type' => 'sectionend' ),
+			array('type' => 'sectionend'),
 
 		);
 	}
-
-
 }
--- includes/Handlers/MetaExtension.php
+++ includes/Handlers/MetaExtension.php
@@ -12,15 +12,19 @@
 
 namespace WooCommerce\Facebook\Handlers;
 
-defined( 'ABSPATH' ) or exit;
+defined('ABSPATH') or exit;
 
 use WooCommerce\Facebook\Handlers\Connection;
+use WP_REST_Request;
+use WP_REST_Response;
+use WP_Error;
 
-class MetaExtension {
+class MetaExtension
+{
 
 	/** @var string Client token */
 	const CLIENT_TOKEN = '195311308289826|52dcd04d6c7ed113121b5eb4be23b4a7';
-    const APP_ID = '474166926521348';
+	const APP_ID = '474166926521348';
 	/** @var string Business name */
 	const BUSINESS_NAME = 'WooCommerce';
 
@@ -29,7 +33,10 @@ class MetaExtension {
 	 *
 	 * @since 2.0.0
 	 */
-	public function __construct() {
+	public function __construct()
+	{
+		add_action('wp_ajax_wc_facebook_update_tokens', array(__CLASS__, 'ajax_update_fb_settings'));
+		add_action('rest_api_init', array(__CLASS__, 'init_rest_endpoint'));
 	}
 
 	/**
@@ -40,7 +47,8 @@ class MetaExtension {
 	 * @param string $external_business_id External business ID.
 	 * @return string
 	 */
-	public static function generateIframeSplashUrl( $is_connected, $plugin, $external_business_id ) {
+	public static function generateIframeSplashUrl($is_connected, $plugin, $external_business_id)
+	{
 		$external_client_metadata = array(
 			'shop_domain'      => wc_get_page_permalink('shop') ?: home_url(),
 			'admin_url'        => admin_url(),
@@ -59,9 +67,126 @@ class MetaExtension {
 				'timezone'                 => 'America/Los_Angeles',
 				'external_business_id'     => $external_business_id,
 				'installed'                => $is_connected,
-				'external_client_metadata' => rawurlencode( json_encode( $external_client_metadata ) ),
+				'external_client_metadata' => rawurlencode(json_encode($external_client_metadata)),
 			),
 			'https://www.commercepartnerhub.com/commerce_extension/splash/'
 		);
 	}
-} 
+
+	/**
+	 * AJAX endpoint to update Facebook settings with authenticated tokens.
+	 *
+	 * Expects POST parameters:
+	 *  - nonce: security nonce.
+	 *  - access_token: system user access token.
+	 *  - merchant_access_token: merchant access token.
+	 *  - page_access_token: page access token.
+	 *  - product_catalog_id: product catalog ID (optional).
+	 *  - pixel_id: pixel ID (optional).
+	 *
+	 * @return void JSON response.
+	 */
+	public static function ajax_update_fb_settings()
+	{
+		// Ensure the current user can manage WooCommerce settings.
+		if (! current_user_can('manage_woocommerce')) {
+			wp_send_json_error(array('message' => __('Unauthorized request', 'facebook-for-woocommerce')));
+		}
+
+		// Validate the nonce.
+		$nonce = isset($_POST['nonce']) ? sanitize_key(wp_unslash($_POST['nonce'])) : '';
+		if (empty($nonce) || ! wp_verify_nonce($nonce, 'wc_facebook_ajax_token_update')) {
+			wp_send_json_error(array('message' => __('Invalid nonce', 'facebook-for-woocommerce')));
+		}
+
+		// Sanitize and retrieve POST data.
+		$access_token          = isset($_POST['access_token']) ? sanitize_text_field(wp_unslash($_POST['access_token'])) : '';
+		$merchant_access_token = isset($_POST['merchant_access_token']) ? sanitize_text_field(wp_unslash($_POST['merchant_access_token'])) : '';
+		$page_access_token     = isset($_POST['page_access_token']) ? sanitize_text_field(wp_unslash($_POST['page_access_token'])) : '';
+		$product_catalog_id    = isset($_POST['product_catalog_id']) ? sanitize_text_field(wp_unslash($_POST['product_catalog_id'])) : '';
+		$pixel_id              = isset($_POST['pixel_id']) ? sanitize_text_field(wp_unslash($_POST['pixel_id'])) : '';
+
+		// Validate required tokens.
+		if (empty($access_token) || empty($merchant_access_token) || empty($page_access_token)) {
+			wp_send_json_error(array('message' => __('Missing required token data', 'facebook-for-woocommerce')));
+		}
+
+		// Update Facebook settings via options.
+		update_option('wc_facebook_access_token', $access_token);
+		update_option('wc_facebook_merchant_access_token', $merchant_access_token);
+		update_option('wc_facebook_page_access_token', $page_access_token);
+		update_option('wc_facebook_product_catalog_id', $product_catalog_id);
+		update_option('wc_facebook_pixel_id', $pixel_id);
+
+		wp_send_json_success(array('message' => __('Facebook settings updated successfully', 'facebook-for-woocommerce')));
+	}
+
+	/**
+	 * REST API endpoint initialization.
+	 *
+	 * @return void
+	 */
+	public static function init_rest_endpoint()
+	{
+		register_rest_route(
+			'wc-facebook/v1',
+			'update_tokens',
+			array(
+				'methods'             => 'POST',
+				'callback'            => array(__CLASS__, 'rest_update_fb_tokens'),
+				'permission_callback' => array(__CLASS__, 'rest_update_fb_tokens_permission_callback'),
+			)
+		);
+	}
+
+	/**
+	 * Permission callback for the REST API endpoint.
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 * @return bool
+	 */
+	public static function rest_update_fb_tokens_permission_callback($request)
+	{
+		return current_user_can('manage_woocommerce');
+	}
+
+	/**
+	 * REST API endpoint callback to update Facebook settings.
+	 *
+	 * Expects POST parameters:
+	 *  - nonce: security nonce.
+	 *  - access_token: system user access token.
+	 *  - merchant_access_token: merchant access token.
+	 *  - page_access_token: page access token.
+	 *  - product_catalog_id: product catalog ID (optional).
+	 *  - pixel_id: pixel ID (optional).
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 * @return WP_REST_Response|WP_Error
+	 */
+	public static function rest_update_fb_tokens(WP_REST_Request $request)
+	{
+		// Get JSON data from request body
+		$params = $request->get_json_params();
+
+		// Sanitize and retrieve data
+		$access_token          = isset($params['access_token']) ? sanitize_text_field($params['access_token']) : '';
+		$merchant_access_token = isset($params['merchant_access_token']) ? sanitize_text_field($params['merchant_access_token']) : '';
+		$page_access_token     = isset($params['page_access_token']) ? sanitize_text_field($params['page_access_token']) : '';
+		$product_catalog_id    = isset($params['product_catalog_id']) ? sanitize_text_field($params['product_catalog_id']) : '';
+		$pixel_id              = isset($params['pixel_id']) ? sanitize_text_field($params['pixel_id']) : '';
+
+		if (empty($access_token) || empty($merchant_access_token) || empty($page_access_token)) {
+			return new WP_Error('missing_token', __('Missing required token data', 'facebook-for-woocommerce'), array('status' => 400));
+		}
+
+		// Update the options
+		update_option('wc_facebook_access_token', $access_token);
+		update_option('wc_facebook_merchant_access_token', $merchant_access_token);
+		update_option('wc_facebook_page_access_token', $page_access_token);
+		update_option('wc_facebook_product_catalog_id', $product_catalog_id);
+		update_option('wc_facebook_pixel_id', $pixel_id);
+
+		return new WP_REST_Response(array('success' => true, 'message' => __('Facebook settings updated successfully', 'facebook-for-woocommerce')), 200);
+	}
+}
