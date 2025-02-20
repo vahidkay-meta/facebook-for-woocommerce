49f438d1ea2cb98516827e640786a1e90a88d5e9
David Evbodaghe <davidevbodaghe@meta.com>
2025-02-20 18:10:41 +0000

    Fix FB Products Data Tab

--- includes/Products/FBCategories.php
+++ includes/Products/FBCategories.php
@@ -22,7 +22,17 @@ defined( 'ABSPATH' ) || exit;
  */
 class FBCategories {
 
-	private $keys_to_exclude = ['brand' => true, 'color' => true, 'material' => true, 'gender' => true, 'condition' => true, 'size' => true, 'colour' => true, 'age_group' => true, 'pattern' => true ];
+	private $keys_to_exclude = [
+		'brand'     => true,
+		'color'     => true,
+		'material'  => true,
+		'gender'    => true,
+		'condition' => true,
+		'size'      => true,
+		'colour'    => true,
+		'age_group' => true,
+		'pattern'   => true,
+	];
 
 	/**
 	 * Fetches the attribute from a category using attribute key.
