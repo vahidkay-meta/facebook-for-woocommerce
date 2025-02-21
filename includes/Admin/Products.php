29e8dcf8d816c456e10df9900e8cf5081213e25f
David Evbodaghe <davidevbodaghe@meta.com>
2025-02-21 14:30:09 +0000

    Fix FB Products Data Tab

--- includes/Admin/Products.php
+++ includes/Admin/Products.php
@@ -105,7 +105,6 @@ class Products {
 				name="<?php echo esc_attr( self::FIELD_GOOGLE_PRODUCT_CATEGORY_ID ); ?>"
 				value="<?php echo esc_attr( Products_Handler::get_google_product_category_id( $product ) ); ?>"
 			/>
-			<?php echo wc_help_tip( __( 'Choose the Google product category and (optionally) sub-categories associated with this product.', 'facebook-for-woocommerce' ) ); ?>
 		</p>
 		<?php
 	}
