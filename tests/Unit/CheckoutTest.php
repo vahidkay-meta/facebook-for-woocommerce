e735202ad97c9656646a836e6538c55d0c2ca7f8
Angelo Ou <angeloou@meta.com>
2025-02-25 19:32:00 -0500

    Add flush of rewrite rules

--- tests/Unit/CheckoutTest.php
+++ tests/Unit/CheckoutTest.php
@@ -9,70 +9,82 @@ use WooCommerce\Facebook\Checkout;
 class CheckoutTest extends WP_UnitTestCase {
 
     /**
-     * Checkout instance being tested.
+     * Instance of the Checkout class.
      *
      * @var Checkout
      */
-    private $checkout;
+    protected $checkout;
 
     /**
-     * Sets up the test case.
+     * Set up the test environment.
+     *
+     * @return void
      */
     public function setUp(): void {
         parent::setUp();
         $this->checkout = new Checkout();
+        $this->checkout->add_checkout_permalink_rewrite_rule();
+
+        flush_rewrite_rules();
     }
 
     /**
-     * Tests that hooks are added correctly.
+     * Test if hooks are added correctly.
+     *
+     * @return void
      */
-    public function test_add_hooks() {
-        $this->assertTrue(has_action('init', [$this->checkout, 'add_checkout_permalink_rewrite_rule']) !== false);
-        $this->assertTrue(has_filter('query_vars', [$this->checkout, 'add_checkout_permalink_query_var']) !== false);
-        $this->assertTrue(has_filter('template_include', [$this->checkout, 'load_checkout_permalink_template']) !== false);
+    public function test_hooks_are_added() {
+        $this->assertGreaterThan(0, has_action('init', [$this->checkout, 'add_checkout_permalink_rewrite_rule']));
+        $this->assertGreaterThan(0, has_filter('query_vars', [$this->checkout, 'add_checkout_permalink_query_var']));
+        $this->assertGreaterThan(0, has_filter('template_include', [$this->checkout, 'load_checkout_permalink_template']));
     }
 
     /**
-     * Tests that query vars are added correctly.
+     * Test if query vars are added correctly.
+     *
+     * @return void
      */
     public function test_add_checkout_permalink_query_var() {
-        $vars = ['existing_var'];
-        $new_vars = $this->checkout->add_checkout_permalink_query_var($vars);
+        $vars = apply_filters('query_vars', []);
 
-        $this->assertContains('fb_checkout', $new_vars);
-        $this->assertContains('products', $new_vars);
-        $this->assertContains('coupon', $new_vars);
+        $this->assertContains('fb_checkout', $vars);
+        $this->assertContains('products', $vars);
+        $this->assertContains('coupon', $vars);
     }
 
     /**
-     * Tests that the rewrite rule is added correctly.
+     * Test if rewrite rules are added correctly.
+     *
+     * @return void
      */
     public function test_add_checkout_permalink_rewrite_rule() {
         global $wp_rewrite;
-        $this->checkout->add_checkout_permalink_rewrite_rule();
+
+        $wp_rewrite->flush_rules();
         $rules = $wp_rewrite->wp_rewrite_rules();
 
+        $this->assertIsArray($rules, 'Rewrite rules should be an array');
         $this->assertArrayHasKey('^fb-checkout/?$', $rules);
         $this->assertEquals('index.php?fb_checkout=1', $rules['^fb-checkout/?$']);
     }
 
     /**
-     * Tests that the flush rewrite rules are called on activation.
+     * Test if rewrite rules are flushed on activation.
+     *
+     * @return void
      */
     public function test_flush_rewrite_rules_on_activation() {
         $this->checkout->flush_rewrite_rules_on_activation();
-        // Assuming flush_rewrite_rules() is a global function, we can't directly test its effect here.
-        // You would need to mock or spy on this function to verify it was called.
-        $this->assertTrue(true);
+        $this->assertTrue(did_action('flush_rewrite_rules'));
     }
 
     /**
-     * Tests that the flush rewrite rules are called on deactivation.
+     * Test if rewrite rules are flushed on deactivation.
+     *
+     * @return void
      */
     public function test_flush_rewrite_rules_on_deactivation() {
         $this->checkout->flush_rewrite_rules_on_deactivation();
-        // Assuming flush_rewrite_rules() is a global function, we can't directly test its effect here.
-        // You would need to mock or spy on this function to verify it was called.
-        $this->assertTrue(true);
+        $this->assertTrue(did_action('flush_rewrite_rules'));
     }
 }
